f742a06dbd220267dffa37fe9c3f302ab8cefa8c
crummy82 <bradleycrum@gmail.com>
2023-06-23 10:30:22 -0700

    Update .wezterm-windows.lua

--- .wezterm-windows.lua
+++ .wezterm-windows.lua
@@ -1,27 +1,21 @@
 local wezterm = require 'wezterm'
+local config = {}
+--local wsl_domains = wezterm.default_wsl_domains()
 
--- WSL only
-local wsl_domains = wezterm.default_wsl_domains()
+config.audible_bell = 'Disabled'
+config.color_scheme = 'Default Dark (base16)'
+config.enable_scroll_bar = true
+config.font = wezterm.font 'Hack Nerd Font Mono'
+config.font_size = 12.0
+config.force_reverse_video_cursor = true
+config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 120
+config.initial_rows = 26
+config.prefer_egl = true
+config.scrollback_lines = 10000
+config.show_update_window = true
+config.use_dead_keys = false
+config.window_close_confirmation = 'NeverPrompt'
+-- config.wsl_domains = wsl_domains -- WSL only
 
-return { 
-  audible_bell = 'Disabled',
-  color_scheme = "Dracula (Official)",
-  colors = {
-    ansi = {"#21222C", "#FF5555", "#50FA7B", "#F1FA8C", "#8866FF", "#FF79C6", "#8BE9FD", "#F8F8F2"},
-    brights = {"#6272A4", "#FF6E6E", "#69FF94", "#FFFFA5", "#D6ACFF", "#FF92DF", "#A4FFFF", "#FFFFFF"}
-  },
-  
-  default_prog = {'wsl.exe'}, -- WSL only
-  enable_scroll_bar = true,
-  font = wezterm.font 'Consolas',
-  font_size = 12.0,
-  force_reverse_video_cursor = true,
-  hide_tab_bar_if_only_one_tab = true,
-  initial_cols = 120,
-  initial_rows = 26,
-  scrollback_lines = 10000,
-  show_update_window = true,
-  use_dead_keys = false,
-  window_close_confirmation = 'NeverPrompt',
-  wsl_domains = wsl_domains -- WSL only
-}
+return config

f282c0ce1e6eaaf81ec165d4b0972ab272389d55
crummy82 <bradleycrum@gmail.com>
2023-06-28 09:29:41 -0400

    Update .wezterm-windows.lua
    Updated WSL and font config

--- .wezterm-windows.lua
+++ .wezterm-windows.lua
@@ -1,21 +1,21 @@
 local wezterm = require 'wezterm'
 local config = {}
---local wsl_domains = wezterm.default_wsl_domains()
 
 config.audible_bell = 'Disabled'
 config.color_scheme = 'Default Dark (base16)'
 config.enable_scroll_bar = true
-config.font = wezterm.font 'Hack Nerd Font Mono'
+-- config.font = wezterm.font 'Hack Nerd Font Mono'
+config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 12.0
 config.force_reverse_video_cursor = true
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 120
 config.initial_rows = 26
-config.prefer_egl = true
+-- config.prefer_egl = true
 config.scrollback_lines = 10000
 config.show_update_window = true
 config.use_dead_keys = false
 config.window_close_confirmation = 'NeverPrompt'
--- config.wsl_domains = wsl_domains -- WSL only
+config.default_domain = 'WSL:Ubuntu'
 
 return config

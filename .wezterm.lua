48a26e205f5e364baef9ea0a2e4e0a2eb0fa1cb2
crummy82 <bradleycrum@gmail.com>
2024-01-27 17:02:48 -0500

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,8 +6,8 @@ config.audible_bell = 'Disabled'
 config.color_scheme = 'Catppuccin Mocha'
 -- config.color_scheme = "Dracula (Official)",
 config.enable_scroll_bar = true
-config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 12.0
+config.font = wezterm.font 'MesloLGSDZ Nerd Font'
+config.font_size = 11.0
 config.force_reverse_video_cursor = true
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 112
@@ -16,6 +16,7 @@ config.scrollback_lines = 10000
 config.show_update_window = true
 config.use_dead_keys = false
 config.window_close_confirmation = 'NeverPrompt'
+config.warn_about_missing_glyphs = false
 
 wezterm.on('gui-startup', function()
   local tab, pane, window = mux.spawn_window{}
